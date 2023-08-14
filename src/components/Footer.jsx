050b262386e806d547e5a7a906e81b9e557abb9e
Ayush Singh <74094464+ayush292003@users.noreply.github.com>
2023-08-14 11:38:22 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>Copyright ©2023 by Ayush All Rights Reserved.</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <h1>Text Editor and Reader</h1>
+      </header>
+    </>
+  )
+}
+
+export default Header
